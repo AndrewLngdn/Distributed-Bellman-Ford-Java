b315f55ddaa50eb68ab9c88b7e6cd0a5e6078bb8
Andrew Langdon <andrewlngdn@gmail.com>
2013-12-14 19:12:28 -0500

    test packet goes to first client

--- Bfclient.java
+++ Bfclient.java
@@ -2,10 +2,12 @@ import java.io.*;
 import java.net.*;
 import java.util.Hashtable;
 import java.util.ArrayList;
+
 class Bfclient {
 	static int localport;
 	static int timeout; // seconds
 	static boolean execute;
+	static DatagramSocket localSock;
 	private BufferedReader input = null;
 	static ArrayList<Hashtable<String, String>> neighborsInfo 
 		= new ArrayList<Hashtable<String, String>>();
@@ -14,17 +16,39 @@ class Bfclient {
 		execute = true;
 
 		interpretArgs(args);
-		// setupSockets();
+		setupSockets();
 		listenToSocket();
 		dealWithTimeouts();
 		listenForCommands();
 	}
 
+	public static void setupSockets(){
+		//listening socket
+		try {
+			localSock = new DatagramSocket(localport);
+		} catch (SocketException e){
+			e.printStackTrace();
+		}
+	}
+
 	public static void listenToSocket(){
 		Thread t = new Thread(new Runnable(){
 			public void run(){
-				int count = 0;
 				while(execute){
+					byte[] buffer = new byte[4096];
+					DatagramPacket rpack = new DatagramPacket(buffer, buffer.length);
+					
+					System.out.println("waiting to recieve");
+					try {
+						localSock.receive(rpack);
+						System.out.println("Received");
+					} catch (IOException e){
+						e.printStackTrace();
+					}
+
+					byte[] packet = new byte[rpack.getLength()];
+					System.arraycopy(rpack.getData(), 0, packet, 0, rpack.getLength());
+					System.out.println(new String(packet));
 					//listen for updates from neighbor
 					// update RT
 					// update neighbors
@@ -47,6 +71,29 @@ class Bfclient {
 		t.start();
 	}
 
+	public static void sendPacket(Hashtable<String, String> neighbor, byte[] data){
+		InetAddress remote_addr = null;
+		try {
+			remote_addr = InetAddress.getByName(neighbor.get("ip"));
+		} catch (UnknownHostException e){
+			e.printStackTrace();
+		}
+
+		int port = Integer.parseInt(neighbor.get("port"));
+		System.out.println("sending packet to " + remote_addr);
+		System.out.println("at port " + port);
+
+		DatagramPacket spacket = new DatagramPacket(data, data.length,
+													remote_addr, port);
+		try {
+			localSock.send(spacket);
+		} catch (IOException e){
+			e.printStackTrace();
+		}
+
+	}
+
+
 	public static void interpretArgs(String[] args){
 		if (args.length < 2 || (args.length-2)%3 != 0){
 			System.out.println("Incorrect arguments");
@@ -58,7 +105,7 @@ class Bfclient {
 			System.out.println("localport: " + localport);
 			System.out.println("timeout: " + timeout);
 		}
-		
+
 		// fillout neighbor info
 		for (int i = 2; i < args.length; i += 3){
 			Hashtable<String, String> neighbor 
@@ -70,13 +117,12 @@ class Bfclient {
 			neighborsInfo.add(neighbor);
 		}
 
-		System.out.println("first ip: " + neighborsInfo.get(0).get("ip"));
-		System.out.println("first port: " + neighborsInfo.get(0).get("port"));
-		System.out.println("first weight: " + neighborsInfo.get(0).get("weight"));
-		System.out.println("second ip: " + neighborsInfo.get(1).get("ip"));
-		System.out.println("second port: " + neighborsInfo.get(1).get("port"));
-		System.out.println("second weight: " + neighborsInfo.get(1).get("weight"));
-
+		// System.out.println("first ip: " + neighborsInfo.get(0).get("ip"));
+		// System.out.println("first port: " + neighborsInfo.get(0).get("port"));
+		// System.out.println("first weight: " + neighborsInfo.get(0).get("weight"));
+		// System.out.println("second ip: " + neighborsInfo.get(1).get("ip"));
+		// System.out.println("second port: " + neighborsInfo.get(1).get("port"));
+		// System.out.println("second weight: " + neighborsInfo.get(1).get("weight"));
 	}
 
 	public static void listenForCommands(){
@@ -108,7 +154,7 @@ class Bfclient {
 	private static void interpretCommand(String command){
 		String[] tokens = command.split(" ");
 		if (tokens.length == 2 && tokens[0].equals("LINKDOWN")){
-			System.out.println("linkdown " + tokens[1]);
+			sendPacket(neighborsInfo.get(0), "test 1 2 3".getBytes());
 		} else 
 		if (tokens.length == 2 && tokens[0].equals("LINKUP")){
 			System.out.println("LINKUP " + tokens[1]);
